77bcfe980eaa074311e1500f3b1d4198d89198b7
Gabriel <gnicholauson@gmail.com>
2022-11-30 14:02:05 -0600

    add start game

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+import Score from './Score.js';
+
+const playerInput = document.querySelector('.player-input');
+const currentWord = document.querySelector('.current-word');
+
+const allWords = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
+'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
+'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
+'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
+'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
+'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
+'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
+'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
+'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
+'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
+'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
+'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+'keyboard', 'window'];
+
+/************************
+ * Event listeners
+************************/
+playerInput.addEventListener('click', () => {
+    startGame(...allWords);
+});
+
+
+/************************
+ * Functions
+************************/
+
+function startGame(...words) {
+    currentWord.value = words[randomNum(words.length)];
+}
+
+function randomNum(arrLength) {
+    return Math.floor(Math.random() * arrLength);
+}
